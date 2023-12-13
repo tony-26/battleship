8b5109456544df1d11d029139b263803c8d838e1
tony <wangh9947@gmail.com>
2023-12-13 20:56:25 +0000

    attackAt func

--- src/lib/ShipPart.js
+++ src/lib/ShipPart.js
@@ -1,7 +1,13 @@
 class ShipPart {
   constructor({ position }) {
     this.position = position;
-    
+    this.isDamaged = false;
+  }
+
+  attackAt(coords) {
+    if (coords[0] === this.position[0] && coords[1] === this.position[1]) {
+      this.isDamaged = true;
+    }
   }
 }
 export default ShipPart;
--- src/test/lib/ShipPart.test.js
+++ src/test/lib/ShipPart.test.js
@@ -5,4 +5,12 @@ describe("part", () => {
     expect(part1.position).toEqual([1, 5]);
     expect(part1.isDamaged).toEqual(false);
   });
+
+  it("example2", () => {
+    const part = new ShipPart({ position: [2, 6] });
+    part.attackAt([2, 5]);
+    expect(part.isDamaged).toEqual(false);
+    part.attackAt([2, 6]);
+    expect(part.isDamaged).toEqual(true);
+  });
 });
